fdb4c38d8fbf87dea78607e4f32770dfea2b0831
Nnamdi Enyobi <Nnamdienyobi123@gmail.com>
2026-08-01 22:45:53 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const currentYear = new Date().getFullYear();
+
+const userName = prompt("Enter your name:");
+const userAge = Number(prompt("Enter your age:"));
+
+const birthYear = currentYear - userAge;
+
+alert(`Welcome, ${userName}!
+You are ${userAge} years old.
+You were born around ${birthYear}.`);
+
+console.log("Name:", userName);
+console.log("Age:", userAge);
+console.log("Birth Year:", birthYear);
+
+// Extension
+const favoriteHobby = prompt("What is your favorite hobby?");
+alert(`Hello ${userName}! Your favorite hobby is ${favoriteHobby}.`);
