90b7c7e75f1d0676af0402375e7fbb8ee70592de
nnamdi-security <peter.chizobam@xultechng.com>
2026-08-04 07:56:15 +0100

    create branch and 2 files

---
+++ index.js
@@ -0,0 +1,19 @@
+// Part 1: Group Discussion
+//1. You can use a variable to store the name because variable allows you store an data with a name and use the data later
+//2. console.log() is used to display something to the screen and especially useful when debugging. alert() is used to display a  message to the user to acknowledge. prompt() is used to recieve input from the user while corfim() is used to ask the user for binary decision
+//3. let allow you to declare a variable and reassign it to a different value later while const does not. A programmer may choose const because he doesn't want to accidentally reassign a variable whose value should not change
+//4. Different datatypes help JS to perform different oprations correctly. It is like having different tools for different job. trying to use screw driver to drive down a nail will not work well
+//5. It may cause consistency problem
+
+//PART B: PREDICT THE OUTPUT
+//Snippet A: 25 & twenty-five
+//Snippet B: Is it raining? true
+//Snippet C: Undefined
+
+//PART 3: DEBUGGING CHALLENGE
+//userName didn't end with a semicolon and favoriteColor started with a capital C
+
+//PART 4: The program first need to receive as input the name and age and store it in a variable. The the program will need to subtract the age from the current year to get the year the user is born. The program will need to return the year which then will be assigned to a variable which then will be concatenated (or use template literal) to add display the age and the birth year.
+
+//PART 5: EXTENSION CHALLENGE
+//We use prompt() to receive one more string input and store with a variable. Then concatenate it with the other information
